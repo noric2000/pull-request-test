ea72545e0091191b5ecb47932695febda03ebbb9
noric <noric2000@gmail.com>
2018-09-21 17:33:24 +0900

    厳密等価演算子===を使用するように変更

--- sort.js
+++ sort.js
@@ -1,7 +1,6 @@
-ort.js
 var sortNumber = function (number) {
    number.sort(function (a, b) {
-       if (a == b) {
+       if (a === b) {
            return 0;
        }
        return a < b ? -1 : 1;
